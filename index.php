fc447050adb252fdefdd11caba221e1e6e828213
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-07 11:39:39 +0200

    myfirstpush

--- index.php
+++ index.php
@@ -5,8 +5,24 @@
  <body>
  <style> body {background-color: #c4a484;} </style>
  <form action="process.php"  method="post">
+    Enter your ID <input id="id" type="text">
+    <input type="submit">   
+ </form>
+ <form action="process.php"  method="post">
     Enter your name <input name="name" type="text">
-    <input type="submit">
+    <input type="submit">   
+ </form>
+ <form action="process.php"  method="post">
+    Enter your surname <input name="name" type="text">
+    <input type="submit">   
+ </form>
+ <form action="process.php"  method="post">
+    Enter your email address <input name="name" type="text">
+    <input type="submit">   
+ </form>
+ <form action="process.php"  method="post">
+    Enter your cellphone number <input name="name" type="text">
+    <input type="submit">   
  </form>
  </body>
 </html>
