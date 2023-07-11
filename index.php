562be264fbf693d08ed84b8ef29fac3ac78b8b81
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-11 09:11:17 +0200

    myfirstpush

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
  <body>
  <style> body {background-color: #c4a484;} </style>
  <form action="process.php"  method="post">
-    Enter your ID <input id="id" type="text">
-    Enter your name <input name="name" type="text">
-    Enter your surname <input surname="surname" type="text">
-    Enter your email address <input emailaddress="emailaddress" type="text">
-    Enter your cellphone number <input cellno="cellno" type="text">
+    Enter your ID: <input id="id" type="text"></br>
+    Enter your name: <input name="name" type="text"></br>
+    Enter your surname: <input surname="surname" type="text"></br>
+    Enter your email address: <input emailaddress="emailaddress" type="text"></br>
+    Enter your cellphone number: <input cellno="cellno" type="text"></br>
     <input type="submit">    
  </form>
  </body>
