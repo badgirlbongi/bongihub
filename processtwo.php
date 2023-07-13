6222782e5584fdc33bcd8618bee1a309cd31a409
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-13 13:04:43 +0200

    myfirstpush

---
+++ processtwo.php
@@ -0,0 +1,16 @@
+// testing user POST request
+//13:00 13.07.2023
+
+$errors = [];
+if(array_key_exists('register', $_POST))
+{
+    $data = 
+           [
+            'id' => isset($_POST['user']['id'])?$_POST[''user']['id'] :'',
+            'name' => isset($_POST['user']['name'])?$_POST['user']['name'] :'',
+            'surname' => isset($_POST['user']['surname'])?$_POST['user']['surname'] :'',
+            'emailaddress' => isset($_POST['user']['emailaddress'])?$_POST['user']['emailaddress'] :'',
+            'cellno' => isset($_POST['user']['cellno'])?$_POST['user']['cellno'] :'',
+            'password' => isset($_POST['user']['password'])?$_POST['user']['password'] :'',
+           ]
+}
