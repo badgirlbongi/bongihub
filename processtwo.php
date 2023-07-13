db313f00ea7755468019bf7dc3cd2ee76ae079fb
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-13 13:13:50 +0200

    myfirstpush

--- processtwo.php
+++ processtwo.php
@@ -1,3 +1,4 @@
+<?php
 // testing user POST request
 //13:00 13.07.2023
 
@@ -6,12 +7,14 @@ if(array_key_exists('register', $_POST))
 {
     $data = 
            [
-            'id' => isset($_POST['user']['id'])?$_POST[''user']['id'] :'',
+            'id' => isset($_POST['user']['id'])?$_POST['user']['id'] :'',
             'name' => isset($_POST['user']['name'])?$_POST['user']['name'] :'',
             'surname' => isset($_POST['user']['surname'])?$_POST['user']['surname'] :'',
             'emailaddress' => isset($_POST['user']['emailaddress'])?$_POST['user']['emailaddress'] :'',
             'cellno' => isset($_POST['user']['cellno'])?$_POST['user']['cellno'] :'',
             'password' => isset($_POST['user']['password'])?$_POST['user']['password'] :'',
-           ]
-           
+           ];
+
 }
+
+?>
