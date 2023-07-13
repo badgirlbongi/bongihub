63a7624222c9fe28468f7a92e41dce1382e1e905
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-13 12:07:27 +0200

    myfirstpush

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  <body>
  <style> body {background-color: #c4a484;} </style>
  <form action="process.php"  method="post">
+    //12:06 13.07.2023
     Enter your ID: <input name="id" type="text"></br>
     Enter your name: <input name="name" type="text"></br>
     Enter your surname: <input name="surname" type="text"></br>
