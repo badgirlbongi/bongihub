35a16d6677867da7c49ad2a6bce08f9bc9c65084
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-17 11:45:08 +0200

    myfirstpush

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 
-<?php /*
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "gitbongi";
+//$dbname = "gitbongi";
 
 //creating a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,6 +12,18 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error)
 {
    die("connection failed: " .$conn->connect_error);
+}
+
+//09:00 17.07.2023
+//Create database
+$sql = "CREATE DATABASE gitbongi";
+if ($conn->query($sql) === TRUE)
+{
+    echo "Database created successfully";
+}
+else
+{
+    echo "Error creating database: ". $conn->error;
 }
 
 //creating tables
@@ -35,7 +47,7 @@ else
 
 //populating rows
 $sql = "INSERT INTO account(id, firstname, lastname,emailaddress, phonenumber)
-        //VALUES()";
+    VALUES(default,'bongisiwe', 'shabalala','bongishabz21@gmail.com','0825947973')";
 
 if ($conn->query($sql) === TRUE)
 {
@@ -47,5 +59,38 @@ else
    echo "Error: ". $sql."<br>". $conn->error;
 }
 
-$conn->close();*/
+//09:00 17.07.2023
+//get id of the last inserted record
+if ($conn->query(sql) === TRUE)
+{
+    $last_id = $conn->insert_id;
+    echo "New record created successfully. Last inserted ID is: ". $last_id;
+}
+else
+{
+    echo"Error: ". $sql. "<br>" . $conn->error;
+}
+
+//10:00 17.07.2023
+//inserting multiple records
+$sql = "INSERT INTO gitbongi (firstname, lastname, email)
+VALUES ('Thabiso', 'Seerati', 'thabisoT@gmail.com');";
+$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Ayanda', 'More', 'ayandamore@gmail.com');";
+$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Mandisa', 'Shabalala', 'mandishabz@gmail.com')";
+
+if ($conn->multi_query($sql) === TRUE)
+{
+  echo "New records created successfully";
+}
+else
+{
+    echo "Error: ". $sql. "<br>" . $conn->error;
+}
+
+//php mysql prepared statements
+
+
+$conn->close();
 ?>
