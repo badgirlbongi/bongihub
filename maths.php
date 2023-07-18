452dab3c58c8fa242f2d32d08fd9982c9168d363
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-18 15:00:28 +0200

    3pm/18.07.2023

---
+++ maths.php
@@ -0,0 +1,29 @@
+<?php
+//18.07.2023
+//pi function
+echo(pi()); // returns 3.1415926535898
+
+//minn() and max() Functions
+echo(min(0, 150, 30, 20, -8, -200));  // returns -200
+echo(max(0, 150, 30, 20, -8, -200));  // returns 150
+
+//abs() function returns the absolute (positive) value of a number
+echo(abs(-8.7));  // returns 8.7
+
+//finding the square root
+echo(sqrt(64));  // returns 8
+
+//function rounds a floating-point number to its nearest integer
+echo(round(0.60));  // returns 1
+echo(round(0.49));  // returns 0
+
+//rand() function generates a random number
+echo(rand());
+echo(rand(10, 100)); //getting a random number between 10 and 100
+
+//acos() function returns the arc cosine of a number
+echo(acos(0.64) . "<br>");
+echo(acos(-0.4) . "<br>");
+echo(acos(-1) . "<br>");//returns PI
+
+?>
