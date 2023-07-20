cd8b9481a9d0384f1cfbf8a9bbad8354dbcb8aeb
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-20 15:46:11 +0200

    updated class

--- model/model.php
+++ model/model.php
@@ -1,5 +1,6 @@
 <?php
-class Client 
+/*
+final class Client 
 {
   // Properties
   public $name;
@@ -8,7 +9,7 @@ class Client
   public $cellno;
 
   // Methods
-  function set_name($name)
+ function set_name($name)
   { $this->name = $name;}
 
   function get_name() 
@@ -30,7 +31,70 @@ class Client
   { $this->cellno = $cellno;}
 
   function get_cellno()
-  { return $this->cellno;}
+  { return $this->cellno;}*/
+?>
 
+<?php
+
+final class Client{
+private $name;
+private $surname;
+private $email;
+private $cellno;
+
+function __construct(){}
+
+public function setParam(stdClass $client){
+
+    if(array_key_exists('name', $client)){
+        $this->name = $client->name;
+    }
+
+    if(array_key_exists('surname',$client)){
+        $this->surname = $client->surname;
+    }
+
+    if(array_key_exists('email', $client)){
+        $this->email = $client->email;
+    }
+
+    if(array_key_exist('cellno', $client)){
+        $this->cellno = $client->institution;
+    }
 }
+
+public function register( array $client)
+{
+    $errors = [];
+
+    if(trim($client['name'])){
+        $this->name = $client['name'];
+    }
+    else{
+        $errors[] = new ValidatorError('name','name cannot be empty');
+    }
+
+    if(trim($client['surname'])){
+        $this->surname = $client['surname'];
+    }
+    else{
+        $errors[] = new ValidatorError('surname','surname cannot be empty');
+    }
+
+    if(trim($client['email'])){
+        $this->email = $client['email'];
+    }
+    else{
+        $errors[] = new ValidatorError('email','email cannot be empty');
+    }
+    
+    if(trim($client['cellno'])){
+        $this->cellno = $client['cellno'];
+    }
+    else{
+        $errors[] = new ValidatorError('cellno','cellphone cannot be empty');
+    } 
+
+    return $errors;
+}}
 ?>
