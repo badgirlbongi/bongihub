06b33e5cccc2a62551a719b2a667496dfd14b09b
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-21 14:29:15 +0200

    2-3pm/21.07.2023

---
+++ new_separate_project/login.php
@@ -0,0 +1,39 @@
+<?php 
+ session_start();
+
+   $_SESSION;
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+</head>
+<body>
+     <style type="text/css">
+        #text
+        {
+            height: 25px;
+            border-radius: 5px;
+            padding: 4px;
+            border: solid thin #aaa;
+        }
+        #button
+        {
+            padding: 10px;
+            width: 100px;
+            color: white;
+            background-color: lightblue;
+            border: none;           
+        }
+        #box
+        {
+           background-color: grey;
+           margin: auto;
+           width: 300px;
+           padding: 20px; 
+
+        }
+    </style>
+</body>
+</html>
