1997c80a708a30245d7f0a17fec20604bd86a9c6
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-25 14:18:08 +0200

    working on the firsttest project step by step 2pm/25.07.2023

---
+++ project_four/index.php
@@ -0,0 +1,33 @@
+<?php
+/** 
+ * controller class Api and access point
+ * @category    Controller
+ * @package     FirstTest
+ * @author      RamakhanyaD <techcodehive@gmail.com>
+ * @license     openSource 
+ * @link        https://revtech.co.za
+ */
+
+//exception which causes HTTP ERROR 404(Not Found)
+class NotFoundException extends Exception {}
+
+//Application controller and main access point
+final class Index
+{
+    const LAYOUT_DIR = '';
+    const PAGE_DIR = '';
+    const LAYOUT_PAGE = '';
+    const DEFAULT_PAGE = '';
+
+    //static $class is where you declare all classes created outside index.php form
+    private static $CLASS = 
+    [
+
+    ];
+
+    //system configurations
+    
+
+
+}
+
