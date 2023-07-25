191253fb049b0e73d8b470af533b1460a76fae55
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-25 15:05:55 +0200

    3pm/25.07.2023

--- project_four/index.php
+++ project_four/index.php
@@ -26,7 +26,38 @@ final class Index
     ];
 
     //system configurations
-    
+    function __construct()
+    {
+        // error reporting - all errors for development (ensure you have display_errors = On in your php.ini file)
+        error_reporting(E_ALL | E_STRICT);
+        mb_internal_encoding('UTF-8');
+        set_exception_handler([$this, 'handleException']);
+        spl_autoload_register([$this, 'loadClass']);
+    }
+
+    //class loader
+    public function loadClass($name)
+    {
+        if (!array_key_exists($name, self::$CLASS)) 
+        { die('Class "' . $name . '" not found.'); }
+        require_once __DIR__.self::$CLASS[$name];
+    }
+
+    //execption handler
+    public function handleException($ex)
+    {
+        $extra = ['message' => $ex->getMessage()];
+        if ($ex instanceof NotFoundException) 
+        { header('HTTP/1.0 404 Not Found');
+          $this->runPage('404', $extra);
+        } 
+        else 
+        { // TODO log exception
+          header('HTTP/1.1 500 Internal Server Error');
+          $this->runPage('500', $extra);
+        }
+    }
+
 
 
 }
