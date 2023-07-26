d2ee29c1f07df5ecf7c23caa987b03f9fab6dea3
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-26 14:15:40 +0200

    2-3pm/26.07.2023

--- project_two/web/view.php
+++ project_two/web/view.php
@@ -1,10 +1,6 @@
 <?php
 //using class created in model.php
 $user = new Client;
-$user->set_name('$user_name');
-$user->set_surname('$user_surname');
-$user->set_email('$user_email');
-$user->set_cellno('$user_cellno');
 
 if($_SERVER['REQUEST_METHOD'] === 'POST')
 { 
@@ -12,6 +8,11 @@ if($_SERVER['REQUEST_METHOD'] === 'POST')
     $user_surname =$_POST["surname"];
     $user_email =$_POST["email"];
     $user_cellno =$_POST["cellno"];
+
+    $user->set_name('$user_name');
+    $user->set_surname('$user_surname');
+    $user->set_email('$user_email');
+    $user->set_cellno('$user_cellno');
   
 }
 ?>
