01967179d19c8167fba49b2292f64608d7bb97d9
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-07-26 14:30:59 +0200

    2-3pm/26.07.2023

--- project_two/web/view.php
+++ project_two/web/view.php
@@ -9,10 +9,10 @@ if($_SERVER['REQUEST_METHOD'] === 'POST')
     $user_email =$_POST["email"];
     $user_cellno =$_POST["cellno"];
 
-    $user->set_name('$user_name');
-    $user->set_surname('$user_surname');
-    $user->set_email('$user_email');
-    $user->set_cellno('$user_cellno');
+    $user->set_name("$user_name");
+    $user->set_surname("$user_surname");
+    $user->set_email("$user_email");
+    $user->set_cellno("$user_cellno");
   
 }
 ?>
