a1e19ab875394c7b1ea602dc00ebc8a83e4946c8
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-08-03 15:05:10 +0200

    python-15:05/03.08.2023

---
+++ python/firstproject.py
@@ -0,0 +1,12 @@
+#installed python on my pc
+#also installed python extentions on my vscode
+#my first project(how to comment)
+#declare a variable
+y = "Hello,python!"
+print(y)
+print("I am Bongi, and this is my first python project")
+#indentation is important in python especially for arguments
+#for example the if statement
+#no semicolons needed just colons
+if 5>2:
+   print("Five is greater than two") 
