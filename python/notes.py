8366e916326a8649c2dd1904765c59fc0e6b182b
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-08-04 09:32:48 +0200

    python_notes-09am/04.08.2023

--- python/notes.py
+++ python/notes.py
@@ -1,7 +1,7 @@
 #installed python on my pc
 #also installed python extentions on my vscode
 #my first project(how to comment)
-#declare a variable
+#no command for declaring variables
 y = "Hello,python!"
 print(y)
 print("I am Bongi, and this is my first python project")
@@ -10,3 +10,8 @@ print("I am Bongi, and this is my first python project")
 #no semicolons needed just colons
 if 5>2:
    print("Five is greater than two") 
+#If you want to specify the data type of a variable use casting.
+x = str(3)   
+y = int(3)    
+z = float(3)
+
