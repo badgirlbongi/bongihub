9697faa331cc443a481e5dd26a3fb768d0574e3d
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-08-07 09:09:18 +0200

    python-09am/07.08.2023

--- python/notes.py
+++ python/notes.py
@@ -184,4 +184,23 @@ def my_function5(**kid):
 
 my_function5(fname = "Nhlanhla", lname = "Shabalala")
 
+#matpotlib is a library for plotting graphs
+#an installation is needed for it to be used
+#also it is not important for my project i was just trying something out
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o')
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
 
