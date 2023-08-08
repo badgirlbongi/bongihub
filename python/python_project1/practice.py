31bb76fbc7a06466737d653638104b91c329928b
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-08-08 13:47:46 +0200

    if statement-13:50/08.08.2023

--- python/python_project1/practice.py
+++ python/python_project1/practice.py
@@ -5,3 +5,18 @@ print("\033[31m","No offense but that's really OLD!")
 print()
 print("\033[0m","So you are ","\033[35m",myName," ",mySurname,"\033[0m"," and you are ","\033[32m",myAge,"\033[0m","years old")
 print("\033[33m","nice to meet you ","\033[35m",myName,"\033[0m")
+print()
+choice = input("Do you have a pet? ")
+if choice =="yes":
+    myPet = input("Is your pet a dog or a cat? ")
+    if myPet == "dog":
+        print("welcome to the woof! gang ",myName," together with your pet")
+    if myPet =="cat":
+        print("welcome to the meow! gang ",myName," together with your pet")
+    else : 
+        print("Oops!,looks like your pet isn't in our list")
+else:
+    print("You should really get a pet ",myName)
+
+
+  
