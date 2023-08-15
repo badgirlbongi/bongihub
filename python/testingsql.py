5442eeef51917cc6215ec28f4ad88c7cca23c78d
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-08-15 13:01:34 +0200

    sqlitepython-13:00/08.15.2023

--- python/testingsql.py
+++ python/testingsql.py
@@ -4,6 +4,13 @@ import sqlite3
 conn = sqlite3.connect('members.db')
 cur = conn.cursor()
 
+#load SQL script from file
+with open("test.sql") as file:
+    sql_script = file.read()
+
+#execute script
+cur.executescript(sql_script)
+
 #display data
 member_data = cur.execute("SELECT * FROM members ORDER BY ln")
 for row in member_data:
