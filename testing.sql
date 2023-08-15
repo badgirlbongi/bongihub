4ad75edf3bc30034747aafa271392bd2be6e9b12
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-08-15 12:08:27 +0200

    sqlitepython-12:00/15.08.2023

--- testing.sql
+++ testing.sql
@@ -1,9 +1,9 @@
 BEGIN;
 CREATE TABLE members(id, fn, ln);
-INSERT INTO members(1,"Naume", "Malinga");
-INSERT INTO members(2,"Bongisiwe", "Shabalala");
-INSERT INTO members(3,"Kgotsofalo", "Malinga");
-INSERT INTO members(4,"Nokuthula", "Sosibo");
-INSERT INTO members(5,"Ayanda", "More");
-INSERT INTO members(6,"Thendo", "Maringa");
+INSERT INTO members VALUES(1,"Naume", "Malinga");
+INSERT INTO members VALUES(2,"Bongisiwe", "Shabalala");
+INSERT INTO members VALUES(3,"Kgotsofalo", "Malinga");
+INSERT INTO members VALUES(4,"Nokuthula", "Sosibo");
+INSERT INTO members VALUES(5,"Ayanda", "More");
+INSERT INTO members VALUES(6,"Thendo", "Maringa");
 COMMIT;
