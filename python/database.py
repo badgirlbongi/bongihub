c5cf949516d04c094e45d582dd0bb88861c2c3b2
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-08-15 10:31:34 +0200

    sqlitepython-10:30/15.08.2023

--- python/database.py
+++ python/database.py
@@ -3,5 +3,14 @@ import sqlite3
 #connect to a (new) database
 conn = sqlite3.connect("namesample.db")
 
+#create a cursor
+cur = conn.cursor()
+
+#create a "people" table
+cur.execute('''CREATE TABLE IF NOT EXISTS people
+               (firstname TEXT, lastname TEXT)''')
+conn.commit()
+
 #close connection
+cur.close()
 conn.close()
