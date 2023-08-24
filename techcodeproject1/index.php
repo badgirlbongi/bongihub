fd00a3e3f4312bb7d123fb38923b7fc77b92289e
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-08-24 10:15:47 +0200

    icon_bar example-10:15/24.08.2023

---
+++ techcodeproject1/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {margin:0;}
+
+.icon-bar {
+  width: 100%;
+  background-color: #555;
+  overflow: auto;
+}
+
+.icon-bar a {
+  float: left;
+  width: 16.6%;
+  text-align: center;
+  padding: 12px 0;
+  transition: all 0.3s ease;
+  color: white;
+  font-size: 30px;
+}
+
+.icon-bar a:hover {
+  background-color: #000;
+}
+
+.active {
+  background-color: #04AA6D;
+}
+</style>
+<body>
+
+<div class="icon-bar">
+  <a href="#"><i class="fa fa-bars"></i> Menu</a>
+  <a class="active" href="#"><i class="fa fa-home"></i> Home</a> 
+  <a href="#"><i class="fa fa-search"></i> Search</a> 
+  <a href="#"><i class="fa fa-envelope"></i> Chat</a> 
+  <a href="#"><i class="fa fa-globe"></i> News</a>
+  <a href="#"><i class="fa fa-question"></i> About</a>
+</div>
+
+</body>
+</html> 
