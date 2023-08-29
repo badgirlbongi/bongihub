930f2994dfb08827522eae509af869da6db0f53b
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-08-29 09:01:48 +0200

    parameters_python-09:00/29.08.2023

---
+++ python/parameters.py
@@ -0,0 +1,9 @@
+def whichCake(ingredient):
+    if ingredient == 'chocolate':
+        print("Mmm, chocolate cake is amazing ")
+    elif ingredient == 'brocolli':
+        print("You what mate?!")
+    else:
+        print("Yeah, that's great I suppose__")
+
+whichCake("chocolate")
