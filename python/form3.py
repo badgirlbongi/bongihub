ae214204af3967b3a73263c6167b9e41b25c459d
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-09-04 13:57:46 +0200

    14:00/04.09.2023

--- python/form3.py
+++ python/form3.py
@@ -8,7 +8,7 @@ frame.pack()
 
 #saving user info
 user_info_frame = tkinter.LabelFrame(frame, text = "User Information")
-user_info_frame.grid(row = 0, column = 0)
+user_info_frame.grid(row = 0, column = 0, padx = 20, pady = 20)
 
 first_name_label = tkinter.Label(user_info_frame, text = "First Name")
 first_name_label.grid(row = 0, column = 0)
@@ -19,6 +19,10 @@ first_name_entry = tkinter.Entry(user_info_frame)
 last_name_entry = tkinter.Entry(user_info_frame)
 first_name_entry.grid(row = 1, column = 0)
 last_name_entry.grid(row = 1, column = 0)
+
+title = tkinter.Label(user_info_frame, text = "Title")
+
+
 
 
 window.mainloop()
