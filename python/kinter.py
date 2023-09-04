e9b95fec164ca14193bb615b6d1398bb3ca5eeaa
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-09-04 12:02:58 +0200

    12:00/04.09.2023

---
+++ python/kinter.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os 
+
+root = tk.TK()
+
+canvas = tk.Canvas(root, height = 600, width = 400,bg="#263d42")
+canvas.pack()
+
+frame = tk.Frame(root, bg="#3e646c")
+frame.place()
