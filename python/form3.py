e616396eb1743d22efb89a737890802f99f5eceb
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-09-04 13:10:22 +0200

    13:10/04.09.2023

---
+++ python/form3.py
@@ -0,0 +1,21 @@
+import tkinter 
+
+window = tkinter.Tk()
+window.title("Data Entry Form")
+
+frame = tkinter.Frame(window)
+frame.pack()
+
+#saving user info
+user_info_frame = tkinter.LabelFrame(frame, text = "User Information")
+user_info_frame.grid(row = 0, column = 0)
+
+first_name_label = tkinter.Label(user_info_frame, text = "First Name")
+first_name_label.grid(row = 0, column = 0)
+last_name_label = tkinter.Label(user_info_frame, text = "Last Name")
+last_name_label.grid(row = 0, column = 1)
+
+first_name_entry = tkinter.Entry(user_info_frame)
+
+
+window.mainloop()
