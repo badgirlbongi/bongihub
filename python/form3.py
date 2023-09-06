b0f2a3aedcef6389c23e59d45b105a234c1f473b
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-09-06 14:12:18 +0200

    tkinter form - 14:13/06.09.2023

--- python/form3.py
+++ python/form3.py
@@ -9,7 +9,7 @@ frame.pack()
 
 #saving user info
 user_info_frame = tkinter.LabelFrame(frame, text = "User Information")
-user_info_frame.grid(row = 0, column = 0, padx = 20, pady = 20)
+user_info_frame.grid(row = 0, column = 0, padx = 20, pady = 10)
 
 first_name_label = tkinter.Label(user_info_frame, text = "First Name")
 first_name_label.grid(row = 0, column = 0)
@@ -40,8 +40,8 @@ for widget in user_info_frame.winfo_children():
     widget.grid_configure(padx = 10, pady = 5)
 
 #saving course info 
-courses_frame = tkinter.Label(frame)
-courses_frame.grid(row = 1, column = 8, sticky = "news", padx = 20, pady = 20)
+courses_frame = tkinter.LabelFrame(frame)
+courses_frame.grid(row = 1, column = 0, sticky = "news", padx = 20, pady = 10)
 
 registered_label = tkinter.Label(courses_frame, text = "Registration Status")
 registered_check = tkinter.Checkbutton(courses_frame, text ="Currently Registered")
@@ -57,5 +57,18 @@ numsemesters_label = tkinter.Label(courses_frame, text = "# Semesters")
 numsemesters_spinbox = tkinter.Spinbox(courses_frame, from_ = 0, to = "infinity")
 numsemesters_label.grid(row = 0, column = 2)
 numsemesters_spinbox.grid(row = 1, column = 2)
+
+for widget in user_info_frame.winfo_children():
+    widget.grid_configure(padx = 10, pady = 5)
+
+#Accept terms
+terms_frame = tkinter.LabelFrame(frame, text = "Terms and Conditions")
+terms_frame.grid(row = 2, column = 0, sticky = "news", padx = 20, pady = 10)
+terms_check = tkinter.Checkbutton(terms_frame, text ="I accept the terms and conditions.")
+terms_check.grid(row = 0, column = 0)
+
+#Button 
+button = tkinter.Button(frame, text = "Enter Data")
+button.grid(row = 3, column = 0, sticky = "news", padx = 20, pady = 10)
 
 window.mainloop()
