ce889d681aa1301e62d9d0d202ca98ba644158e7
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-09-12 11:17:23 +0200

    displaying picture with grid-11:17/12.09.2023

--- projectOne/signup.py
+++ projectOne/signup.py
@@ -1,6 +1,15 @@
 from tkinter import *
 from PIL import ImageTk
 
+root = Tk()
+
+bgImage = ImageTk.PhotoImage(file = 'bg.png')
+
+bgLabel = Label(root, image = bgImage)
+bgLabel.grid(row = 0, column = 0)
+
+
+root.mainloop()
 
 
 
