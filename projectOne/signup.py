5d356fffabada5be3bc17492a1c823a1b7ad1aca
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-09-12 11:35:52 +0200

    display using geometry and place-11:36/12.09.2023

--- projectOne/signup.py
+++ projectOne/signup.py
@@ -2,11 +2,11 @@ from tkinter import *
 from PIL import ImageTk
 
 root = Tk()
-
-bgImage = ImageTk.PhotoImage(file = 'bg.png')
+root.geometry('990x660+50+50')
+bgImage = ImageTk.PhotoImage(file="bg.png")
 
 bgLabel = Label(root, image = bgImage)
-bgLabel.grid(row = 0, column = 0)
+bgLabel.place(x = 0, y = 0)
 
 
 root.mainloop()
