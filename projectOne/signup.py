7c3d4ddee69e3da16a67f5bec8cc8420a714cb5f
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-09-12 11:46:26 +0200

    heading 11:47/12.09.2023

--- projectOne/signup.py
+++ projectOne/signup.py
@@ -3,10 +3,15 @@ from PIL import ImageTk
 
 root = Tk()
 root.geometry('990x660+50+50')
-bgImage = ImageTk.PhotoImage(file="bg.png")
+bgImage = ImageTk.PhotoImage(file='bg.jpg')
 
 bgLabel = Label(root, image = bgImage)
 bgLabel.place(x = 0, y = 0)
+
+heading = Label(root, text = 'USER LOGIN', font = ('Microsoft Yahei UI Light',23,'bold')
+                ,bg = 'white', fg = 'firebrick1')
+heading.place(x = 605,y = 120)
+
 
 
 root.mainloop()
