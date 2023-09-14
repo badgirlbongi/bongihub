36711a02019c91131586fff56013189b2840ee2c
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-09-14 13:20:13 +0200

    signup-13:20/14.09.2023

--- projectOne/signup.py
+++ projectOne/signup.py
@@ -37,13 +37,26 @@ passwordEntry=Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'),
                  fg='white',bg='firebrick1')
 passwordEntry.grid(row=6,column=0,sticky='w',padx=25)
 
-
-confirmPassLabel=Label(frame,text='Username',font=('Microsoft Yahei UI Light',10,'bold'),
+confirmPassLabel=Label(frame,text='Confirm Password',font=('Microsoft Yahei UI Light',10,'bold'),
                  bg='white',fg='firebrick1')
 confirmPassLabel.grid(row=7,column=0,sticky='w',padx=25,pady=(10,0))
 confirmPassEntry=Entry(frame,width=25,font=('Microsoft Yahei UI Light',10,'bold'),
                  fg='white',bg='firebrick1')
 confirmPassEntry.grid(row=8,column=0,sticky='w',padx=25)
+
+termsandconditions = Checkbutton(frame, text ='I agree to the Terms & Conditions', 
+                                 font = ('Microsoft Yahei UI Light',10,'bold'),fg='firebrick1',
+                                 bg='white',activebackground='white',activeforeground='firebrick1')
+termsandconditions.grid(row=9,column=0,pady=10,padx=15)
+
+signupButton = Button(frame,text='Signup',font=('Open Sans',16,'bold'),bd=0
+                      ,activebackground='firebrick1',activeforeground='white',width=17)
+signupButton.grid(row=10,column=0,pady=10)
+
+alreadyaccount= Label(frame,text='Create New Account?',font=('Open Sans',9,'bold underline'),
+                          fg='blue',bg='white',activeforeground='blue',
+                          activebackground='white',cursor='hand2',bd=0)
+alreadyaccount.place(row = 11,column=0,sticky='w',padx=25)
 
 
 signup_window.mainloop()
