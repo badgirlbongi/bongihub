3b4b93c9162269dc031b56ef22d0eb317d3012b7
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-09-14 10:12:08 +0200

    signup(heading)-10:12/14.09.2023

--- projectOne/signup.py
+++ projectOne/signup.py
@@ -2,11 +2,22 @@ from tkinter import*
 from PIL import ImageTk
 
 signup_window =Tk()
+signup_window.title('Signup page')
+signup_window.resizable(False,False)
 
 background = ImageTk.PhotoImage(file='bg.jpg')
-
 bgLabel=Label(signup_window,image=background)
 bgLabel.grid()
+
+frame = Frame(signup_window)
+frame.place(x=554,y=100)
+
+heading=Label(frame,text ='CREATE AN ACCOUNT',font =('Microsoft Yahei UI Light',23,'bold'),
+              bg='white',fg='firebrick1')
+heading.grid(row=0,column=0)
+
+
+
 
 signup_window.mainloop()
 
