76e07f7b5595acf8d01a5bd97b582e08df603d68
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-10-10 12:36:57 +0200

    login page - 12:40 10/10/2023

--- python/displaypic.py
+++ python/displaypic.py
@@ -1,5 +1,28 @@
+#from tkinter import *
+#from PIL import ImageTk,Image
+
+#root = Tk()
+#canvas = Canvas(root, width = 300, height = 300)
+#canvas.pack()
+#img = ImageTk.PhotoImage(Image.open("C:\\xampp\\htdocs\\bongihub\\python\\bg.jpg"))
+#canvas.create_image(20, 20, anchor=NW, image=img)
+#root.mainloop
+
+#img = Image.open("C:\\xampp\\htdocs\\bongihub\\python\\bg.jpg")
+#img.thumbnail((300, 300))
+#img = ImageTk.PhotoImage(img)
+
+
+#img = ImageTk.PhotoImage(Image.open("C:\\xampp\\htdocs\\bongihub\\python\\bg.jpg"))
+#canvas.img = img  # Keep a reference
+#canvas.create_image(20, 20, anchor=NW, image=img)
+
+#root.mainloop
+
 from tkinter import *
 from PIL import Image
 
-im = Image.open("C:\xampp\htdocs\bongihub\bg.jpg")
+im = Image.open("C:\\xampp\\htdocs\\bongihub\\python\\projectTwo\\login.png")
 im.show()
+
+input("Press ENTER to exit") # pause
---
+++ python/projectTwo/login.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.title('Login')
+root.geometry('925x500+300+200')
+root.configure(bg="#fff")
+root.resizable(False,False)
+
+img = PhotoImage(file='C:\\xampp\\htdocs\\bongihub\\python\\projectTwo\\login.png')
+Label(root,image=img,bg='white').place(x=50,y=50)
+
+frame= Frame(root,width=350,height=350,bg="white")
+frame.place(x=480,y=70)
+
+heading=Label(frame,text='Sign in',fg='#57a1f8',bg='white',font=('Microsoft YaHei UI Light',23,'bold'))
+heading.place(x=100,y=5)
+
+#functions
+def on_enter(e):
+    user.delete(0,'end')
+
+def on_leave(e):
+    name = user.get()
+    if name == '':
+        user.insert(0,'Username')
+
+
+#username
+user= Entry(frame,width=25,fg='black',border=0,bg="white",font =('Microsoft YaHei UI Light',11))
+user.place(x=30,y=80)
+user.insert(0,'username')
+user.bind('<FocusIn>', on_enter)
+user.bind('<FocusOut>', on_leave)
+
+Frame(frame,width=295,height=2,bg='black').place(x=25,y=107)
+
+#password
+code=Entry(frame,width=25,fg='black',border=0,bg="white",font =('Microsoft YaHei UI Light',11))
+code.place(x=30,y=150)
+code.insert(0,'Password')
+code.bind('<FocusIn>', on_enter)
+code.bind('<FocusOut>', on_leave)
+
+Frame(frame,width=295,height=2,bg='black').place(x=25,y=177)
+
+#sign-in button
+Button(frame,width=39,pady=7,text='Sign in',bg='#57a1f8',fg='white',border=0).place(x=35,y=204)
+label=Label(frame,text="Don't have an account?",fg='black',bg='white',font=('Microsoft YaHei UI Light',9))
+label.place(x=75,y=270)
+
+sign_up=Button(frame,width=6,text='Sign up',border=0,bg='white',cursor='hand2',fg='#57a1f8')
+sign_up.place(x=215,y=270)
+
+
+root.mainloop()
+input("Press ENTER to exit")
