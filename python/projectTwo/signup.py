9187767b2c30f0703b074635f9b77a3cb62f5337
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-10-11 09:20:51 +0200

    sign-up 09:21 11/10/2023

--- python/projectTwo/signup.py
+++ python/projectTwo/signup.py
@@ -19,17 +19,62 @@ heading.place(x=100,y=5)
 
 #username
 #functions
-
+def on_enter(e):
+    user.delete(0,'end')
+def on_leave(e):
+    if user.get()=='':
+        user.insert(0,'Username')
 
 user=Entry(frame,width=25,fg='black',border=0,bg='white',font=('Microsoft Yahei UI Light',11))
 user.place(x=30,y=80)
 user.insert(0,'Username')
+user.bind("<FocusIn>",on_enter)
+user.bind("<FocusOut>",on_leave)
 
 Frame(frame,width=295,height=2,bg='black').place(x=25,y=107)
 
-
 #password
 #functions
+def on_enter(e):
+    code.delete(0,'end')
+def on_leave(e):
+    if code.get()=='':
+        code.insert(0,'Password')
+
+code=Entry(frame,width=25,fg='black',border=0,bg='white',font=('Microsoft Yahei UI Light',11))
+code.place(x=30,y=150)
+code.insert(0,'Password')
+code.bind("<FocusIn>",on_enter)
+code.bind("<FocusOut>",on_leave)
+
+Frame(frame,width=295,height=2,bg='black').place(x=25,y=177)
+
+#password2
+#functions
+def on_enter(e):
+    confirm_code.delete(0,'end')
+def on_leave(e):
+    if confirm_code.get()=='':
+        confirm_code.insert(0,'Confirm Password')
+
+confirm_code=Entry(frame,width=25,fg='black',border=0,bg='white',font=('Microsoft Yahei UI Light',11))
+confirm_code.place(x=30,y=220)
+confirm_code.insert(0,'Confirm Password')
+confirm_code.bind("<FocusIn>",on_enter)
+confirm_code.bind("<FocusOut>",on_leave)
+
+Frame(frame,width=295,height=2,bg='black').place(x=25,y=247)
+
+#signup button
+Button(frame,width=39,pady=7,text='Sign up',bg='#57a1f8',fg='white',border=0).place(x=35,y=280)
+label=Label(frame,text='I havean account',fg='black',bg='white',font=('Microsoft YaHei UI Light',9))
+label.place(x=90,y=340)
+
+signin=Button(frame,width=6,text ='Sign in',border=0,bg='white',cursor='hand2',fg='#57a1f8')
+signin.place(x=200,y=340)
+
+####################--------------------------------------------------######################
+
 
 window.mainloop()
 input("Press ENTER to exit")
