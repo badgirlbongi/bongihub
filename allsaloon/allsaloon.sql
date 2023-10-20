84f38939604cff5aa52fa579d8ae3f412a163492
badgirlbong <bongisiwesharmaineshabalala@gmail.com>
2023-10-20 11:03:55 +0200

    updated erd and database

--- allsaloon/allsaloon.sql
+++ allsaloon/allsaloon.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2023 at 09:43 AM
+-- Generation Time: Oct 20, 2023 at 10:58 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,21 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `card`
---
-
-CREATE TABLE `card` (
-  `customer_email` varchar(90) NOT NULL,
-  `card_type` text NOT NULL,
-  `savings_cheque` text NOT NULL,
-  `primary_acc_num` int(25) NOT NULL,
-  `security_code` int(7) NOT NULL,
-  `branch_code` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `customer`
 --
 
@@ -49,6 +34,18 @@ CREATE TABLE `customer` (
   `lastname` text NOT NULL,
   `password1` int(30) NOT NULL,
   `password2` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paymentdetails`
+--
+
+CREATE TABLE `paymentdetails` (
+  `customer_email` varchar(90) NOT NULL,
+  `payment_method` text NOT NULL,
+  `payment_token` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -85,16 +82,16 @@ CREATE TABLE `service` (
 --
 
 --
--- Indexes for table `card`
---
-ALTER TABLE `card`
-  ADD PRIMARY KEY (`customer_email`);
-
---
 -- Indexes for table `customer`
 --
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paymentdetails`
+--
+ALTER TABLE `paymentdetails`
+  ADD PRIMARY KEY (`customer_email`);
 
 --
 -- Indexes for table `saloon`
