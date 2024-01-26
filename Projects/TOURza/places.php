e22fe8589d59ce174918e9bb102bd58d14f6b4f4
badgirlbongi <bongisiwesharmaineshabalala@gmail.com>
2024-01-26 15:48:49 +0200

    updated workflow

--- Projects/TOURza/places.php
+++ Projects/TOURza/places.php
@@ -1,68 +1,63 @@
 <?
-$page = 'EC';
+$province = 'EC';
 
-function generateContent($page) {
+function generateContent($province) {
     
-    switch ($page) {
+    switch ($province) {
         case 'EC':
           $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'EC'";
-          $image =;
-          $description =;
-          $link =;
-             break; 
-      case 'FS':
-        $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'FS'";
-          $image =;
-          $description =;
-          $link =;
-       break;
-       case 'G':
-        $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'G'";
-        $image =;
-        $description =;
-        $link =;
-        break;
+          break; 
+        case 'FS':
+          $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'FS'";
+          break;
+        case 'G':
+          $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'G'";
+          break;
         case 'KZN':
           $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'KZN'";
-          $image =;
-          $description =;
-          $link =;
           break;
         case 'L':
           $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'L'";
-          $image =;
-          $description =;
-          $link =;
           break;
         case 'MP':
           $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'MP'";
-          $image =;
-          $description =;
-          $link =;
           break;
         case 'NC':
           $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'NC'";
-          $image =;
-          $description =;
-          $link =;
           break;
         case 'NW':
           $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'NW'";
-          $image =;
-          $description =;
-          $link =;
           break;
         case 'WC':
-          echo 'this is working';
+          $sql = "SELECT placePicture, placeDescription, placeLink FROM place WHERE provinceID = 'WC'";
           break;
       default:
         // Handle other cases or provide a default behavior
         break;
     }
     
+     // Execute the SQL query
+    $result = mysqli_query($your_db_connection, $sql);
+
+    if ($result) {
+      // Fetch the data
+      $row = mysqli_fetch_assoc($result);
+
+      // Assign values to variables
+      $image = $row['placePicture'];
+      $description = $row['placeDescription'];
+      $link = $row['placeLink'];
+
+      // Use the variables as needed
+    } else {
+      // Handle the case where the query fails
+    }
     
   }
 ?>
+
+
+
 
 <!--
 php
