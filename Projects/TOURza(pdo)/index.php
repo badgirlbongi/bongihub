71cf0d62457ad13f6c55bb5fdf54e0b4c8fc4997
badgirlbongi <bongisiwesharmaineshabalala@gmail.com>
2024-02-13 09:56:43 +0200

    updated work flow

--- Projects/TOURza(pdo)/index.php
+++ Projects/TOURza(pdo)/index.php
@@ -796,18 +796,18 @@ function countPlacesByProvinceID($provinceID) {
   <div class="container">
 
   <section class="py-5 text-center container">
-    <div class="row py-lg-5">
-      <div class="col-lg-6 col-md-8 mx-auto">
-      <p class="lead text-body-secondary">Can't find the place you're looking for?, Just search for it.</p>
-      </div>
-      <div class="container-fluid">
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-warning" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </section>
+        <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+                <p class="lead text-body-secondary">Can't find the place you're looking for?, Just search for it.</p>
+            </div>
+            <div class="container-fluid">
+                <form class="d-flex" action="search.php" method="GET">
+                    <input class="form-control me-2" type="search" name="q" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-warning" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </section>
 
     <p class="float-end mb-1">
       <a href="#">Back to top</a>
--- workflow.md
+++ workflow.md
@@ -242,4 +242,4 @@ objectives : when you click a button on the index.phtml for example for free sta
 
 13/02/2024
 + moved the <div> for displaying the pop up message on the index.php to the <main>, now doing testing to see if it will show, still not showing but the place is added
-+ 
++ added a search.php file for the search form: the goal is for it to search through the whole system for keywords entered by the user
