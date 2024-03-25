2d366d57aff7889b7d95614e77d690051c13cb22
badgirlbongi <bongisiwesharmaineshabalala@gmail.com>
2024-03-25 15:18:33 +0200

    updated work flow

--- megan/bot.py
+++ megan/bot.py
@@ -9,3 +9,10 @@ class TradingBot():
         self.log = LogWrapper("TradingBot")
         self.trade_pairs = Settings.get_pairs()
         self.settings = Settings.load_settings()
+        self.log_message(f"Bot started with\n {pprint.pformat(self.settings)}")
+        
+    def log_message(self, msg):
+        self.log.logger.debug(msg)
+
+if __name__ == "__main__":
+    b = TradingBot()
