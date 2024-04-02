8ea6fc97be48452cd8a8454bc4c11782afb5e249
badgirlbongi <bongisiwesharmaineshabalala@gmail.com>
2024-04-02 16:32:38 +0200

    updated trade_manager.py

--- megan/trade_manager.py
+++ megan/trade_manager.py
@@ -8,3 +8,9 @@ class TradeManager():
     def log_message(self, msg):
         if self.log is not None:
             self.log.logger.debug(msg)
+
+    def close_trades(self, pairs_to_close):
+        open_trades, ok = self.api.open_trades()
+            
+    def place_trades(self, trades_to_make):
+        self.log_message(f"TradeManager:place_trades() {trades_to_make}")
