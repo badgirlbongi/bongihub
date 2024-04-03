9bca263acbb11db0df655155005ba6f8f7c6d7ab
badgirlbongi <bongisiwesharmaineshabalala@gmail.com>
2024-04-03 10:31:25 +0200

    updated work flow

--- tradingbot/bot.py
+++ tradingbot/bot.py
@@ -10,3 +10,31 @@ def signal_generator(df):
     close = df.Close.iloc[-1]
     previous_open = df.Open.iloc[-2]
     previous_close = df.Close.iloc[-2]
+
+    #Bearish Pattern
+    if (open > close and 
+    previous_open < previous_close and 
+    close < previous_open and open >= previous_close):
+        return 1
+    
+    #Bullish Pattern
+    elif (open < close and
+    previous_open > previous_close and
+    close > previous_open and
+    open <= previous_close):
+        return 2
+    
+    #No clear pattern
+    else:
+        return 0
+    
+signal = []
+signal.append(0)
+for i in range(1, len(dataF)):
+    df = dataF[i-1:i+1]
+    signal.append(signal_generator(df))
+#signal_generator(data)
+dataF["signal"] = signal
+
+dataF.Ssignal.value_counts()
+#dataF.iloc[:, :]
